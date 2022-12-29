bee3b80eef693673fb7dabc81bbcdd9954079088
Vera Prigodich <vera.prigodich@gmail.com>
2022-12-29 04:37:34 +0300

    код Хемминга

---
+++ лр 4/Lab4/Lab4/HammingCode.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    abstract class HammingCode
+    {
+        public static string TextReader(string path)
+        {
+            string str;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                str = reader.ReadToEnd().ToLower();
+            }
+            return str;
+        }
+
+        public static string ConvertToBinary(string str)
+        {
+            string binStr = null;
+            foreach (char c in str)
+            {
+                if (c != ' ')
+                {
+                    binStr += Convert.ToString(c, 2).PadLeft(8, '0');
+                }
+            }
+            return binStr;
+        }
+
+        public static int Factorial(int n)
+        {
+            if (n == 1)
+                return 1;
+            else 
+                return n * Factorial(n - 1);
+        }
+
+        public static int NewtonBinom(int wt, int r)
+        {
+            return Factorial(r) / (Factorial(wt) * Factorial(r - wt));
+        }
+
+        static public char[,] MatrixA(int r, int k)
+        {
+            int countOfColomn = 0;
+            int wt = 2;
+
+            char[,] matrixA = new char[r, k];
+            while (true)
+            {
+                int amountAllCombination = NewtonBinom(wt, r);
+                int rightCombination = 0;
+                int number = 3;
+
+                while (rightCombination != amountAllCombination && countOfColomn != k)
+                {
+
+                    string binaryNumber = Convert.ToString(number, 2);
+                    int binaryNumberLength = binaryNumber.Length;
+
+                    //Счетчик единиц в бинарной строке
+                    int unitCounter = 0;
+
+                    for (int i = 0; i < binaryNumberLength; i++)
+                    {
+                        if (binaryNumber[i] == '1')
+                            unitCounter++;
+                    }
+
+                    if (unitCounter == wt)
+                    {
+                        countOfColomn++;
+
+                        if (binaryNumberLength < r)
+                        {
+                            for (int j = 0; j < (r - binaryNumberLength); j++)
+                            {
+                                binaryNumber = "0" + binaryNumber;
+                            }
+                        }
+
+                        for (int i1 = 0; i1 < r; i1++)
+                        {
+                            matrixA[i1, countOfColomn - 1] = binaryNumber[i1];
+                        }
+
+                        rightCombination++;
+                    }
+                    number++;
+                }
+
+                if (countOfColomn != k)
+                    wt++;
+                else
+                    break;
+            }
+            return matrixA;
+        }
+
+        static public char[,] MatrixI(int r)
+        {
+            char[,] matrixI = new char[r, r];
+
+            int k = 0;
+            for (int i = k; i < r; i++)
+            {
+                for(int j = 0; j < r; j++)
+                {
+                    if (j == i - k)
+                        matrixI[i, j] = '1';
+                    else
+                        matrixI[i, j] = '0';
+                }
+            }
+            return matrixI;
+        }
+
+        static public char[,] HammingMatrix(int r, int k)
+        {
+            char[,] hammingMatrix = new char[r, r + k];
+            char[,] matrixA = MatrixA(r, k);
+            char[,] matrixI = MatrixI(r);
+
+            for (int i = 0; i < k; i++)
+            {
+                for (int j = 0; j < r; j++)
+                {
+                    hammingMatrix[j, i] = matrixA[j, i];
+                }
+            }
+            int i1 = 1;
+            for (int i = k; i < (k + r); i++)
+            {
+                for (int j = 0; j < r; j++)
+                {
+                    hammingMatrix[j, i] = matrixI[j, i1 - 1];
+                }
+                i1++;
+            }
+            return hammingMatrix;
+        }
+
+        static public string XOR(string a, string b)
+        {
+            if (a.Length < b.Length)
+            {
+                a = a.PadLeft(b.Length, '0');
+            }
+            else if (a.Length > b.Length)
+            {
+                b = b.PadLeft(a.Length, '0');
+            }
+            string result = null;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if ((a[i] == '0' && b[i] == '0') || (a[i] =='1' && b[i] == '1'))
+                {
+                    result += '0';
+                }
+                else
+                {
+                    result += '1';
+                }
+            }
+            return result;
+        }
+
+        static public string RedundancySymbols(string Xk, char[,] hammingMatrix, int r, int k)
+        {
+            string Xr = null;
+            int pairs = 0;
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < k; j++)
+                {
+                    if (hammingMatrix[i, j] == '1' && Xk[j] == '1')
+                        pairs++;
+                }
+                if (pairs % 2 == 0)
+                    Xr += '0';
+                else
+                    Xr += '1';
+                pairs = 0;
+            }
+            return Xr;
+        }
+
+        static public string RandomError(string Xk, int amount)
+        {
+            char[] Yk = new char[Xk.Length];
+            Yk = Xk.ToArray<char>();
+            int previosPosition = 100;
+            int currentPosition;
+
+            for (int i = 0; i < amount; i++)
+            {
+                Random random = new Random();
+                currentPosition = random.Next(0, Xk.Length);
+                while (currentPosition == previosPosition)
+                {
+                    currentPosition = random.Next(0, Xk.Length);
+                }
+                Yk[currentPosition] = char.Parse(XOR(Yk[currentPosition].ToString(), '1'.ToString()));
+                Thread.Sleep(100);
+                previosPosition = currentPosition;
+            }
+            return new string(Yk);
+        }
+
+        static public string ErrorVector(int position, int n)
+        {
+            string errorVector = null;
+            for (int i = 0; i < n; i++)
+            {
+                if (i != position)
+                    errorVector += '0';
+                else
+                    errorVector += '1';
+            }
+            return errorVector;
+        }
+
+        static public int ErrorPosition(char[,] hammingMatrix, string syndrome, int r, int k)
+        {
+            int amountCoincidences = 0;
+            for (int i = 0; i < k; i++)
+            {
+                for (int j = 0; j < r; j++)
+                {
+                    if (hammingMatrix[j, i] == syndrome[j])
+                        amountCoincidences++;
+                }
+
+                if (amountCoincidences == r)
+                    return i;
+                else
+                    amountCoincidences = 0;
+            }
+            return -1;
+        }
+
+
+    }
+}
---
+++ лр 4/Lab4/Lab4/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*1. На основе информационного сообщения, представленного
+            символами русского/английского алфавитов, служебными символами и цифрами, 
+            содержащегося в некотором текстовом файле
+            сформировать информационное сообщение в двоичном виде; 
+            длина сообщения в бинарном виде должна быть не менее 16 символов*/
+            string origXk, Xk;
+            Console.WriteLine("Текст из файла: " + (origXk = HammingCode.TextReader(@"D:\учеба\ЗИиНИС\Лабораторные\лр 4\Lab4\test.txt")));
+            Console.WriteLine("\nИнформационное сообщение в двоичном виде: " + (Xk = HammingCode.ConvertToBinary(origXk)));
+            /*2. Для полученного информационного слова построить проверочную матрицу Хемминга*/
+            int k = Xk.Length;
+            int r = (int)Math.Log(k, 2) + 1;
+
+            Console.WriteLine("\nПроверочная матрица Хемминга:");
+            char[,] hammingMatrix = HammingCode.HammingMatrix(r, k);
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < (k + r); j++)
+                {
+                    Console.Write(hammingMatrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            /*3. вычислить избыточные символы (слово Xr). */
+            string Xr;
+            Console.WriteLine("\nИзбыточные слово: " +(Xr = HammingCode.RedundancySymbols(Xk, hammingMatrix, r, k)));
+            string Xn = string.Concat(Xk, Xr);
+            Console.WriteLine("Кодовое слов: " + Xn);
+            /*4. Принять исходное слово со следующим числом ошибок: 0, 1, 2. 
+             Позиция ошибки определяется (генерируется) случайным образом. */
+            string Yn = Xn;
+            string Yr, errorVector;
+            Console.WriteLine("\nЧисло ошибок - 0, Yn: " + Yn);
+            /*5. Для полученного слова Yn = Yk, Yr, используя уже известную проверочную матрицу Хемминга, 
+             * вновь вычислить избыточные символы (обозначим их Yr’)*/
+            Console.WriteLine("Проверочные символы Y`r: " + (Yr = HammingCode.RedundancySymbols(Xk, hammingMatrix, r, k)));
+            /*6. Вычислить синдром.
+             Если анализ синдрома показал, что информационное сообщение было
+            передано с ошибкой (или 2 ошибками), сгенерировать унарный
+            вектор ошибки Еn = е1, е2, …, еn и исправить одиночную ошибку*/
+            Console.WriteLine("Синдром: " + HammingCode.XOR(Yr, Xr));
+
+            string Yk = HammingCode.RandomError(Xk, 1);
+            Console.WriteLine("\nЧисло ошибок - 1, Yn: " + string.Concat(Yk, Xr));
+            Console.WriteLine("Проверочные символы Y`r: " + (Yr = HammingCode.RedundancySymbols(Yk, hammingMatrix, r, k)));
+            Console.WriteLine("Синдром: " + HammingCode.XOR(Yr, Xr));
+            Console.WriteLine("Вектор ошибки: " + (errorVector = HammingCode.ErrorVector(HammingCode.ErrorPosition(hammingMatrix, HammingCode.XOR(Yr, Xr), r, k), r + k)));
+            Console.WriteLine("Исправленное сообщение: " + HammingCode.XOR(Yk + Xr, errorVector));
+
+            Yk = HammingCode.RandomError(Xk, 2);
+            Console.WriteLine("\nЧисло ошибок - 2, Yn: " + string.Concat(Yk, Xr));
+            Console.WriteLine("Проверочные символы Y`r: " + (Yr = HammingCode.RedundancySymbols(Yk, hammingMatrix, r, k)));
+            Console.WriteLine("Синдром: " + HammingCode.XOR(Yr, Xr));
+            Console.WriteLine("Вектор ошибки: " + (errorVector = HammingCode.ErrorVector(HammingCode.ErrorPosition(hammingMatrix, HammingCode.XOR(Yr, Xr), r, k), r + k)));
+
+            Console.ReadLine();
+        }
+    }
+}
