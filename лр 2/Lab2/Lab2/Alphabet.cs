ef97eb6da8f45136dea2864dde5a1d2c0b93dd0f
Vera Prigodich <vera.prigodich@gmail.com>
2022-12-29 04:36:27 +0300

    энтропия

---
+++ лр 2/Lab2/Lab2/Alphabet.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Lab2
+{
+    public static class Alphabet
+    {
+        public static string TextReader(string path)
+        {
+            string str;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                str = reader.ReadToEnd().ToLower();
+            }
+            return str;
+        }
+
+        public static Dictionary<char, int> LettersDict(string text)
+        {
+            Dictionary<char, int> lettersDict = new Dictionary<char, int>();
+            foreach (char c in text)
+            {
+                if (char.IsDigit(c) || char.IsLetter(c))
+                {
+                    if (!lettersDict.ContainsKey(c))
+                        lettersDict.Add(c, 1);
+                    else lettersDict[c]++;
+                }
+            }
+            return lettersDict;
+        }
+
+        public static Dictionary<char, double> Probs(string text)
+        {
+            Dictionary<char, int> lettersDict = LettersDict(text);
+            Dictionary<char, double> letters = new Dictionary<char, double>();
+            for (int i = 0; i < lettersDict.Values.Count; i++)
+            {
+                letters.Add(lettersDict.Keys.ToArray()[i], (double)lettersDict.Values.ToArray()[i] / lettersDict.Values.Sum());
+            }
+            return letters;
+        }
+
+        public static double Entropy(string text)
+        {
+            double entropy = 0;
+            Dictionary<char, double> lettersProbs = Probs(text);
+            for (int i = 0; i < lettersProbs.Values.Count; i++)
+            {
+                entropy += lettersProbs.Values.ToArray()[i] * Math.Log(lettersProbs.Values.ToArray()[i], 2);
+            }
+            return -entropy;
+        }
+
+        public static string ConvertToAscii(string text)
+        {
+            string asciiTxt = null;
+            foreach (char c in text)
+            {
+                if (char.IsLetter(c))
+                {
+                    asciiTxt += Convert.ToString(c, 2);
+                }
+            }
+            return asciiTxt;
+        }
+
+        public static void Serialize(string path, Dictionary<char, double> probs)
+        {
+            File.Delete(path);
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+               DataContractSerializer formatter = new DataContractSerializer(typeof(Dictionary<char, double>));
+               formatter.WriteObject(fs, probs);
+            }
+        }
+
+        public static double QuantityOfInformation(double entropy, string text)
+        {
+            return entropy * text.Length;
+        }
+
+        public static double MistakeQuantity(double mistakeProb, string text, double entropy)
+        {
+            return text.Length * (entropy - (-mistakeProb * Math.Log(mistakeProb, 2)-(1-mistakeProb)*Math.Log(1-mistakeProb,2)));
+        }
+    }
+}
---
+++ лр 2/Lab2/Lab2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double latEntropy = 0, bulgEntropy = 0, binEntropy = 0;
+            //а) рассчитать энтропию болгароского и латышский
+            //входной параметр провизвольный электронный документ
+            Console.WriteLine("Рассчитать энтропию алфавитов: один – на латинице, другой – на кириллице \n");
+            latEntropy = Alphabet.Entropy(Alphabet.TextReader(@"D:\учеба\ЗИиНИС\Лабораторные\лр 2\Lab2\latvian.txt"));
+            Console.WriteLine("Энтропия латышского: " + latEntropy);
+            bulgEntropy = Alphabet.Entropy(Alphabet.TextReader(@"D:\учеба\ЗИиНИС\Лабораторные\лр 2\Lab2\bulgarian.txt"));
+            Console.WriteLine("Энтропия болгарского: " + bulgEntropy);
+            //частоты появления символов алфавитов оформить в виде гистограмм
+            Alphabet.Serialize(@"D:\учеба\ЗИиНИС\Лабораторные\лр 2\Lab2\probs_latv.xml", Alphabet.Probs(Alphabet.TextReader(@"D:\учеба\ЗИиНИС\Лабораторные\лр 2\Lab2\latvian.txt")));
+            Alphabet.Serialize(@"D:\учеба\ЗИиНИС\Лабораторные\лр 2\Lab2\probs_bulg.xml", Alphabet.Probs(Alphabet.TextReader(@"D:\учеба\ЗИиНИС\Лабораторные\лр 2\Lab2\bulgarian.txt")));
+            //б) определить энтропию бинарного алфавита
+            Console.WriteLine("\nДля входных документов, представленных в бинарных кодах, определить энтропию бинарного алфавита \n");
+            binEntropy = Alphabet.Entropy(Alphabet.ConvertToAscii(Alphabet.TextReader(@"D:\учеба\ЗИиНИС\Лабораторные\лр 2\Lab2\latvian.txt")));
+            Console.WriteLine("Энтропия бинарного для документа на латышском: " + binEntropy);
+            Console.WriteLine("Энтропия бинарного для документа на болгарском: " + Alphabet.Entropy(Alphabet.ConvertToAscii(Alphabet.TextReader(@"D:\учеба\ЗИиНИС\Лабораторные\лр 2\Lab2\bulgarian.txt"))));
+            //в) подсчитать кол-во информации в ФИО на исх. алфавите и кодах ASCII
+            Console.WriteLine("\nПодсчитать количество информации в сообщении, состоящем из собственных фамилии, имени и отчества\n");
+            Console.WriteLine("Количество информации на латышском: " + Alphabet.QuantityOfInformation(latEntropy, "Prigodiča Vera Valerievna"));
+            Console.WriteLine("Количество информации на болгарском: " + Alphabet.QuantityOfInformation(bulgEntropy, "Пригодич Вера Валериевна"));
+            Console.WriteLine("Количество информации в ASCII: " + Alphabet.QuantityOfInformation(binEntropy, Alphabet.ConvertToAscii("Prigodiča Vera Valerievna")));
+            //г) вероятность 0,1
+            Console.WriteLine("\nПри условии, что вероятность ошибочной передачи единичного бита сообщения составляет\n");
+            Console.WriteLine("Количество информации на латышском с вероятностью 0,1: " + Alphabet.MistakeQuantity(0.1, "Prigodiča Vera Valerievna", latEntropy));
+            Console.WriteLine("Количество информации на болгарском с вероятностью 0,1: " + Alphabet.MistakeQuantity(0.1, "Пригодич Вера Валериевна", bulgEntropy));
+            Console.WriteLine("Количество информации в ASCII с вероятностью 0,1: " + Alphabet.MistakeQuantity(0.1, Alphabet.ConvertToAscii("Prigodiča Vera Valerievna"), binEntropy));
+            //вероятность 0,5
+            Console.WriteLine("\nКоличество информации на латышском с вероятностью 0,5: " + Alphabet.MistakeQuantity(0.5, "Prigodiča Vera Valerievna", 1));
+            Console.WriteLine("Количество информации на болгарском с вероятностью 0,5: " + Alphabet.MistakeQuantity(0.5, "Пригодич Вера Валериевна", 1));
+            Console.WriteLine("Количество информации в ASCII с вероятностью 0,5: " + Alphabet.MistakeQuantity(0.5, Alphabet.ConvertToAscii("Prigodiča Vera Valerievna"), 1));
+            //вероятность 1,0
+            Console.WriteLine("\nКоличество информации на латышском с вероятностью 1,0: " + Alphabet.MistakeQuantity(0.999, "Prigodiča Vera Valerievna", latEntropy));
+            Console.WriteLine("Количество информации на болгарском с вероятностью 1,0: " + Alphabet.MistakeQuantity(0.999, "Пригодич Вера Валериевна", bulgEntropy));
+            Console.WriteLine("Количество информации в ASCII с вероятностью 1,0: " + Alphabet.MistakeQuantity(0.999, Alphabet.ConvertToAscii("Prigodiča Vera Valerievna"), binEntropy));
+            Console.ReadLine();
+        }
+    }
+}
